5dbdb2545d2eb91293e20001c9ddd8716cf487f3
Carson Hanel <carsonh14@gmail.com>
2017-11-24 14:49:12 -0600

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -1,8 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 22 21:18:03 2017
+
+@author: Carson Hanel
+
+Perceptron from scratch!
+"""
 import sys
 import getopt
 import os
 import math
-from nltk.corpus import stopwords 
+import pandas as pd
+import csv
 import numpy as np
 
 class Perceptron:
@@ -11,7 +20,7 @@ class Perceptron:
     """
     def __init__(self):
       self.train = []
-      self.test = []
+      self.test  = []
 
   class Example:
     """Represents a document with a label. klass is 'pos' or 'neg' by convention.
@@ -28,33 +37,27 @@ class Perceptron:
     self.words        = {}  #Dictionary for words in the bag of words.
     self.vocab_length = 0   #Total length of the calculated bag of words
     self.count_docs   = 0   #Count of total documents; just an iterator
-    self.bag_of_words = []  #All word occurrences for all documents
     self.weights      = []  #Calculated feature weights
-    self.weights_bi   = []  #Calculated Bigram weights
-    self.weights_tri  = []  #Calculated Trigram weights
-
-  #############################################################################
-  # TODO TODO TODO TODO TODO 
-  # Implement the Perceptron classifier 
-
+    
   def classify(self, words):
     """ TODO
       'words' is a list of words to classify. Return 'pos' or 'neg' classification.
     """
-    words = [word for word in words if word not in stopwords.words('english')]
+    #words = [word for word in words if word not in stopwords.words('english')]
+    #No longer necessary, feature vector
 
     weight = 0.
-    for w in list(words):
+    for w in set(words):
         w = w.lower()
         if w in self.words:
             weight = weight + float(self.weights[int(self.words[w])])
-    print 'The calculated weight is: ', weight
+    #print 'The calculated weight is: ', weight
     if weight > 0:
-        return 'pos'
+        return 0
     else:
-        return 'neg' 
+        return 1 
 
-    return 'pos'
+    return 0
   
 
   def addExample(self, klass, words, doc, iterations):
@@ -70,59 +73,23 @@ class Perceptron:
          y_i is positive or negative depending on the document class. 1 for pos, -1 for neg.
          x_i is the individual feature
     """
-    '''
-    TODOTODOTODO!!!
-    '''
-    klass_int = -1
-    if(klass == 'pos'):
-        klass_int  = 1
+    klass_int = 1
+    if(klass == 1):
+        klass_int = -1
+    occurrence = np.zeros(self.vocab_length)
     
-    #Either true/false based on occurrence
-    #IF NEED TODO: Translate into frequency model vs binary.
-    occurrence     = np.zeros(self.vocab_length)
-    occurrence_bi  = np.zeros(self.vocab_length * 2)
-    occurrence_tri = np.zeros(self.vocab_length * 3) 
-    
-    #Unigram perceptron training:
     for i in range(0, iterations):
         sum_weights = 0.
+        #Set(words) gets the average of the individual words, not of all words in doc.
         for w in set(words):
-            w = w.lower()
             sum_weights += self.weights[int(self.words[w])]
             occurrence[int(self.words[w])] += 1
-        #print 'The sum of weights for doc: ', doc, ' is: ', sum_weights
+        #Changing set(words) to words for feature engineering. More features = more weight
         for w in set(words):
             occur       = occurrence[int(self.words[w])]
             if np.sign(sum_weights * occur) != klass_int:
                 self.weights[int(self.words[w])] += (klass_int - np.sign(sum_weights * occur)) * occur
-                
-    #Bigram perceptron training:
-    #TODO:
-    for i in range(0, iterations):
-        sum_weights = 0.
-        for w in set('''bigrams'''):
-            w = w.lower()
-            sum_weights += self.weights[int(self.words[w])]
-            occurrence_bi[int(self.words[w])] += 1
-        #print 'The sum of weights for doc: ', doc, ' is: ', sum_weights
-        for w in set(words):
-            occur       = occurrence[int(self.words[w])]
-            if np.sign(sum_weights * occur) != klass_int:
-                self.weights[int(self.words[w])] += (klass_int - np.sign(sum_weights * occur)) * occur
-                
-    #Trigram perceptron training:
-    #TODO:
-    for i in range(0, iterations):
-        sum_weights = 0.
-        for w in set('''trigrams'''):
-            w = w.lower()
-            sum_weights += self.weights[int(self.words[w])]
-            occurrence_tri[int(self.words[w])] += 1
-        #print 'The sum of weights for doc: ', doc, ' is: ', sum_weights
-        for w in set(words):
-            occur       = occurrence[int(self.words[w])]
-            if np.sign(sum_weights * occur) != klass_int:
-                self.weights[int(self.words[w])] += (klass_int - np.sign(sum_weights * occur)) * occur
+
     pass
   
   def train(self, split, iterations):
@@ -136,107 +103,81 @@ class Perceptron:
       *  The initial for-loop iterates through the examples given as training data.
       *  From what it seems right now, train is a complete function.
       """
-      
       np.random.shuffle(split.train)
 
       curr_word = 0
       for example in split.train:
           self.count_docs += 1
           for w in example.words:
-              w = w.lower()
+              #w = w.lower()  Not necessary after feature engineering
               if w not in self.words:
                   self.words[w] = curr_word
                   curr_word += 1
       self.vocab_length = len(self.words.keys())
-      self.bag_of_words = np.zeros(self.vocab_length)
+      print 'The vocab length is: %d' % self.vocab_length + '\n'
       self.weights      = np.zeros(self.vocab_length)
-      self.weights_bi   = np.zeros(self.vocab_length * 2);
-      self.weights_tri  = np.zeros(self.vocab_length * 3);
+      
       ex_doc = 0
 
       for example in split.train:
           words = example.words
           self.addExample(example.klass, words, ex_doc, iterations)
           ex_doc += 1
-      
-
-  # END TODO (Modify code beyond here with caution)
-  #############################################################################
-  
-  
-  def readFile(self, fileName):
-    """
-     * Code for reading a file.  you probably don't want to modify anything here, 
-     * unless you don't like the way we segment files.
-    """
-    contents = []
-    f = open(fileName)
-    for line in f:
-      contents.append(line)
-    f.close()
-    result = self.segmentWords('\n'.join(contents)) 
-    return result
-
-  
-  def segmentWords(self, s):
-    """
-     * Splits lines on whitespace for file reading
-    """
-    return s.split()
-
+      pass
   
   def trainSplit(self, trainDir):
-    """Takes in a trainDir, returns one TrainSplit with train set."""
     split = self.TrainSplit()
-    posTrainFileNames = os.listdir('%s/pos/' % trainDir)
-    negTrainFileNames = os.listdir('%s/neg/' % trainDir)
-    for fileName in posTrainFileNames:
-      example = self.Example()
-      example.words = self.readFile('%s/pos/%s' % (trainDir, fileName))
-      example.klass = 'pos'
-      example.words = [word for word in example.words if word not in stopwords.words('english')]
-      split.train.append(example)
-    for fileName in negTrainFileNames:
-      example = self.Example()
-      example.words = self.readFile('%s/neg/%s' % (trainDir, fileName))
-      example.klass = 'neg'
-      example.words = [word for word in example.words if word not in stopwords.words('english')]
-      split.train.append(example)
+    with open(trainDir) as fileName:
+          reader = pd.read_csv(fileName)
+          for index,row in reader.iterrows():
+              content  = row['body']
+              polarity = int(row['fakeness'])
+              example = self.Example()
+              if polarity == 0:
+                  example.words = content
+                  example.klass = polarity
+                  split.train.append(example)
+              if polarity == 1:
+                  example.words = content
+                  example.klass = polarity
+                  split.train.append(example)
     return split
 
-
   def crossValidationSplits(self, trainDir):
-    """Returns a lsit of TrainSplits corresponding to the cross validation splits."""
-    splits = [] 
-    posTrainFileNames = os.listdir('%s/pos/' % trainDir)
-    negTrainFileNames = os.listdir('%s/neg/' % trainDir)
-    #for fileName in trainFileNames:
+    splits   = []
+    content  = str("")
+    polarity = 0
+    
     for fold in range(0, self.numFolds):
-      split = self.TrainSplit()
-      for fileName in posTrainFileNames:
-        example = self.Example()
-        example.words = self.readFile('%s/pos/%s' % (trainDir, fileName))
-        example.words = [word for word in example.words if word not in stopwords.words('english')]
-        example.klass = 'pos'
-        if fileName[2] == str(fold):
-          split.test.append(example)
-        else:
-          split.train.append(example)
-      for fileName in negTrainFileNames:
-        example = self.Example()
-        example.words = self.readFile('%s/neg/%s' % (trainDir, fileName))
-        example.words = [word for word in example.words if word not in stopwords.words('english')]
-        example.klass = 'neg'
-        if fileName[2] == str(fold):
-          split.test.append(example)
-        else:
-          split.train.append(example)
-      splits.append(split)
+        split = self.TrainSplit()
+        count = 0
+        with open(trainDir) as fileName:
+            reader = pd.read_csv(fileName).fillna(value = "")
+            for index,row in reader.iterrows():
+                content  = row['body']
+                polarity = int(row['fakeness'])
+                hashing  = count
+                count    = count + 1
+                example = self.Example()
+                example.words = content.split()
+                example.klass = polarity
+                if hashing != "hash" and hashing % 10 == fold:
+                    split.test.append(example)
+                else:
+                    split.train.append(example)
+        splits.append(split)
     return splits
-  
+'''
+Notes:
+    Reads rows in a CSV file as a dict in order to create test splits.
+    Prepares data to be sent to the perceptron to test validation of
+    feature engineering and perceptron handling.
+'''
 
 def test10Fold(args):
-  pt = Perceptron()
+  pt         = Perceptron()
+  splitName  = "Word_Data"
+  splitCount = 0
   
   iterations = int(args[1])
   splits = pt.crossValidationSplits(args[0])
@@ -257,17 +198,29 @@ def test10Fold(args):
     avgAccuracy += accuracy
     print '[INFO]\tFold %d Accuracy: %f' % (fold, accuracy) 
     fold += 1
+    '''
+    TODO: Beyond here, for every split, create a csv holding weights/words across the corpus
+    '''
+    fileName = splitName + str(splitCount) + ".csv"
+    with open(fileName, 'w') as csvfile:
+        data    = ['word', 'weight'] 
+        archive = csv.DictWriter(csvfile, fieldnames = data)
+        archive.writeheader()
+        for w in set(classifier.words):
+            temp_word   = w
+            temp_weight = classifier.weights[int(classifier.words[w])]
+            archive.writerow({'word' : temp_word, 'weight' : temp_weight})
+        csvfile.close()
+    splitCount += 1
   avgAccuracy = avgAccuracy / fold
   print '[INFO]\tAccuracy: %f' % avgAccuracy
-    
-    
+  
 def classifyDir(trainDir, testDir,iter):
   classifier = Perceptron()
   trainSplit = classifier.trainSplit(trainDir)
   iterations = int(iter)
   classifier.train(trainSplit,iterations)
   testSplit = classifier.trainSplit(testDir)
-  #testFile = classifier.readFile(testFilePath)
   accuracy = 0.0
   for example in testSplit.train:
     words = example.words
@@ -276,7 +229,7 @@ def classifyDir(trainDir, testDir,iter):
       accuracy += 1.0
   accuracy = accuracy / len(testSplit.train)
   print '[INFO]\tAccuracy: %f' % accuracy
-    
+
 def main():
   (options, args) = getopt.getopt(sys.argv[1:], '')
   
