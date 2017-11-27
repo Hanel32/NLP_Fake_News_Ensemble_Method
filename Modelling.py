918882b3e03d56f91621928e9a71c4f674c536e0
Carson Hanel <carsonh14@gmail.com>
2017-11-27 15:09:12 -0600

    Updating the model
    Making the program more collaborative by integrating the perceptron classifier, maxent classifier, BoW/Bigrams/Trigrams with other features.

--- Modelling.py
+++ Modelling.py
@@ -10,19 +10,18 @@ df= pd.read_csv("Complete_DataSet_removed_single_letters.csv")
 df=df[:10]
 def generate_features(h,b):
 
-    X_overlap = gen_or_load_feats(Jaccard_Similarity, h, b)
-    #X_refuting = gen_or_load_feats(refuting_features, h, b)
+    X_overlap  = gen_or_load_feats(Jaccard_Similarity, h, b)
     X_polarity = gen_or_load_feats(polarity_features, h, b)
-    X_hand = gen_or_load_feats(hand_features, h, b)
-    X_vader = gen_or_load_feats(sentiment_feature, h, b)
-    X_NER = gen_or_load_feats(named_entity_feature, h, b)
-    print("X_overlap: ",X_overlap)
-    #print("X_refuting: ",X_refuting)
-    print("X_polarity: ",X_polarity)
-    print("X_hand: ",X_hand,"len:", len(X_hand))
-    print("X_vader: ",X_vader)
-    print("X_NER:",X_NER)
-    X =X_hand+ X_polarity+ X_overlap+X_vader+X_NER
+    X_hand     = gen_or_load_feats(hand_features, h, b)
+    X_vader    = gen_or_load_feats(sentiment_feature, h, b)
+    X_NER      = gen_or_load_feats(named_entity_feature, h, b)
+    #Currently being worked on by Carson:
+    X_unaries  = gen_or_load_feats(unaries, h, b)
+    X_bigrams  = gen_or_load_feats(bigrams, h, b)
+    X_trigrams = gen_or_load_feats(trigrams, h, b)
+    X_percept  = gen_or_load_feats(score, h, b)
+    #Above is current work
+    X          =X_hand+ X_polarity+ X_overlap+X_vader+X_NER
     #X = np.concatenate(X,axis=0)
     print(X)
     return X
